a5ecc0f6789af94dd0ce998d6c2ca11856da968f
Eugene O'Neil <ejo7220@gmail.com>
2020-07-29 20:24:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,12 @@
 def line(array)
+  counter = 1 
   if array.length == 0 
 puts "The line is currently empty."
 else
-  
+  array.each do |name|
+    puts "The line is currently #{counter}, #{name}."
+    counter += 1 
 
   end
-  array
+  
 end
